a10bbfb320be3b6f4e3dfd0ea1d7fb6f30f847e8
朱亮 <teamide@163.com>
2021-12-03 18:38:09 +0800

    Update FormBox.vue

--- html/src/component/FormBox.vue
+++ html/src/component/FormBox.vue
@@ -18,7 +18,58 @@
         auto-focus-button="ok"
         :hide-backdrop="true"
       >
-        <div v-html="one.msg"></div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group
+            id="input-group-1"
+            label="Email address:"
+            label-for="input-1"
+            description="We'll never share your email with anyone else."
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.email"
+              type="email"
+              placeholder="Enter email"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="Your Name:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.name"
+              placeholder="Enter name"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+            <b-form-select
+              id="input-3"
+              v-model="form.food"
+              :options="foods"
+              required
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+            <b-form-checkbox-group
+              v-model="form.checked"
+              id="checkboxes-4"
+              :aria-describedby="ariaDescribedby"
+            >
+              <b-form-checkbox value="me">Check me out</b-form-checkbox>
+              <b-form-checkbox value="that">Check that out</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
       </b-modal>
     </template>
   </div>
@@ -48,7 +99,16 @@ export default {
       let form = config || {};
       form.id = "form-" + this.tool.getRandom();
       form.inputs = form.inputs || [];
-
+      let ok = form.ok;
+      let validate = form.validate;
+      form.ok = () => {
+        if (validate) {
+          if (!validate()) {
+            return;
+          }
+        }
+        ok && ok();
+      };
       form.hide = () => {
         this.del(form);
       };
