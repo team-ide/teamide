7642200d633e93a43caa9dfb8e34f674bddca788
朱亮 <teamide@163.com>
2022-02-23 19:48:27 +0800

    Redis组件添加基础增删改查操作

--- toolbox/redis.go
+++ toolbox/redis.go
@@ -1,6 +1,9 @@
 package toolbox
 
-import "strings"
+import (
+	"encoding/json"
+	"strings"
+)
 
 func GetRedisWorker() *Worker {
 	worker_ := &Worker{
@@ -15,6 +18,17 @@ func GetRedisWorker() *Worker {
 	return worker_
 }
 
+type RedisBaseRequest struct {
+	Key     string `json:"key"`
+	Value   string `json:"value"`
+	Pattern string `json:"pattern"`
+	Size    int    `json:"size"`
+	Type    string `json:"type"`
+	Index   int64  `json:"index"`
+	Count   int64  `json:"count"`
+	Field   string `json:"field"`
+}
+
 func redisWork(work string, config map[string]interface{}, data map[string]interface{}) (res map[string]interface{}, err error) {
 	var service RedisService
 	var address string = config["address"].(string)
@@ -26,12 +40,64 @@ func redisWork(work string, config map[string]interface{}, data map[string]inter
 	if err != nil {
 		return
 	}
+
+	var bs []byte
+	bs, err = json.Marshal(data)
+	if err != nil {
+		return
+	}
+	request := &RedisBaseRequest{}
+	err = json.Unmarshal(bs, request)
+	if err != nil {
+		return
+	}
+
 	res = map[string]interface{}{}
 	switch work {
 	case "get":
 		var valueInfo RedisValueInfo
-		valueInfo, err = service.Get(data["key"].(string))
+		valueInfo, err = service.Get(request.Key)
 		res["value"] = valueInfo
+	case "keys":
+		var count int
+		var keys []string
+		count, keys, err = service.Keys(request.Pattern, request.Size)
+		if err != nil {
+			return
+		}
+		res["count"] = count
+		res["keys"] = keys
+	case "do":
+		if request.Type == "set" {
+			err = service.Set(request.Key, request.Value)
+		} else if request.Type == "sadd" {
+			err = service.Sadd(request.Key, request.Value)
+		} else if request.Type == "srem" {
+			err = service.Srem(request.Key, request.Value)
+		} else if request.Type == "lpush" {
+			err = service.Lpush(request.Key, request.Value)
+		} else if request.Type == "rpush" {
+			err = service.Rpush(request.Key, request.Value)
+		} else if request.Type == "lset" {
+			err = service.Lset(request.Key, request.Index, request.Value)
+		} else if request.Type == "lrem" {
+			err = service.Lrem(request.Key, request.Count, request.Value)
+		} else if request.Type == "hset" {
+			err = service.Hset(request.Key, request.Field, request.Value)
+		} else if request.Type == "hdel" {
+			err = service.Hdel(request.Key, request.Field)
+		}
+		if err != nil {
+			return
+		}
+	case "delete":
+		var count int
+		count, err = service.Del(request.Key)
+		res["count"] = count
+	case "deletePattern":
+		var count int
+		count, err = service.DelPattern(request.Pattern)
+		res["count"] = count
 	}
 	return
 }
--- toolbox/zookeeper.go
+++ toolbox/zookeeper.go
@@ -1,6 +1,7 @@
 package toolbox
 
 import (
+	"encoding/json"
 	"errors"
 	"strings"
 	"time"
@@ -30,44 +31,61 @@ func GetZKWorker() *Worker {
 	return worker_
 }
 
+type ZookeeperBaseRequest struct {
+	Path string `json:"path"`
+	Data string `json:"data"`
+}
+
 func zkWork(work string, config map[string]interface{}, data map[string]interface{}) (res map[string]interface{}, err error) {
 	var service *ZKService
 	service, err = getZKService(config["address"].(string))
 	if err != nil {
 		return
 	}
+
+	var bs []byte
+	bs, err = json.Marshal(data)
+	if err != nil {
+		return
+	}
+	request := &ZookeeperBaseRequest{}
+	err = json.Unmarshal(bs, request)
+	if err != nil {
+		return
+	}
+
 	res = map[string]interface{}{}
 	switch work {
 	case "get":
 		var data []byte
-		data, err = service.Get(config["path"].(string))
+		data, err = service.Get(request.Path)
 		if err != nil {
 			return
 		}
 		res["data"] = string(data)
 	case "save":
 		var isEx bool
-		isEx, err = service.Exists(config["path"].(string))
+		isEx, err = service.Exists(request.Path)
 		if err != nil {
 			return
 		}
 		if isEx {
-			err = service.SetData(config["path"].(string), []byte(config["data"].(string)))
+			err = service.SetData(request.Path, []byte(request.Data))
 		} else {
-			err = service.CreateIfNotExists(config["path"].(string), []byte(config["data"].(string)))
+			err = service.CreateIfNotExists(request.Path, []byte(request.Data))
 		}
 		if err != nil {
 			return
 		}
 	case "getChildren":
 		var isEx bool
-		isEx, err = service.Exists(config["path"].(string))
+		isEx, err = service.Exists(request.Path)
 		if err != nil {
 			return
 		}
 		if isEx {
 			var children []string
-			children, err = service.GetChildren(config["path"].(string))
+			children, err = service.GetChildren(request.Path)
 			if err != nil {
 				return
 			}
@@ -75,12 +93,12 @@ func zkWork(work string, config map[string]interface{}, data map[string]interfac
 		}
 	case "delete":
 		var isEx bool
-		isEx, err = service.Exists(config["path"].(string))
+		isEx, err = service.Exists(request.Path)
 		if err != nil {
 			return
 		}
 		if isEx {
-			err = service.Delete(config["path"].(string))
+			err = service.Delete(request.Path)
 			if err != nil {
 				return
 			}
