e12b38e9cdbc79e8905ecf6cb706fcb31c47a3bf
朱亮 <teamide@163.com>
2023-04-25 19:36:39 +0800

    thrift方法执行添加性能测试，添加测试记录查看分析性能

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/shirou/gopsutil/v3 v3.23.1
 	github.com/team-ide/go-dialect v1.9.0
-	github.com/team-ide/go-tool v0.3.4
+	github.com/team-ide/go-tool v0.3.6
 	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.6.0
 	golang.org/x/net v0.7.0
@@ -29,7 +29,7 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/apache/thrift v0.18.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -73,7 +73,7 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/olivere/elastic/v7 v7.0.32 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
@@ -94,8 +94,8 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
--- internal/module/module_thrift/api.go
+++ internal/module/module_thrift/api.go
@@ -1,11 +1,16 @@
 package module_thrift
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/team-ide/go-tool/task"
 	"github.com/team-ide/go-tool/thrift"
 	"github.com/team-ide/go-tool/util"
+	"io/fs"
+	"os"
 	"strings"
 	"sync"
 	"teamide/internal/module/module_toolbox"
@@ -25,16 +30,22 @@ func NewApi(toolboxService *module_toolbox.ToolboxService) *api {
 
 var (
 	Power                 = base.AppendPower(&base.PowerAction{Action: "thrift", Text: "Thrift", ShouldLogin: true, StandAlone: true})
-	context               = base.AppendPower(&base.PowerAction{Action: "context", Text: "上下文", ShouldLogin: true, StandAlone: true, Parent: Power})
+	contextPower          = base.AppendPower(&base.PowerAction{Action: "context", Text: "上下文", ShouldLogin: true, StandAlone: true, Parent: Power})
 	getMethodArgFields    = base.AppendPower(&base.PowerAction{Action: "getMethodArgFields", Text: "上下文", ShouldLogin: true, StandAlone: true, Parent: Power})
 	invokeByServerAddress = base.AppendPower(&base.PowerAction{Action: "invokeByServerAddress", Text: "上下文", ShouldLogin: true, StandAlone: true, Parent: Power})
+	invokeReports         = base.AppendPower(&base.PowerAction{Action: "invokeReports", Text: "执行报告", ShouldLogin: true, StandAlone: true, Parent: Power})
+	invokeStop            = base.AppendPower(&base.PowerAction{Action: "invokeStop", Text: "执行停止", ShouldLogin: true, StandAlone: true, Parent: Power})
+	invokeInfo            = base.AppendPower(&base.PowerAction{Action: "invokeInfo", Text: "执行信息", ShouldLogin: true, StandAlone: true, Parent: Power})
 	closePower            = base.AppendPower(&base.PowerAction{Action: "close", Text: "关闭", ShouldLogin: true, StandAlone: true, Parent: Power})
 )
 
 func (this_ *api) GetApis() (apis []*base.ApiWorker) {
-	apis = append(apis, &base.ApiWorker{Power: context, Do: this_.context})
+	apis = append(apis, &base.ApiWorker{Power: contextPower, Do: this_.context})
 	apis = append(apis, &base.ApiWorker{Power: getMethodArgFields, Do: this_.getMethodArgFields})
 	apis = append(apis, &base.ApiWorker{Power: invokeByServerAddress, Do: this_.invokeByServerAddress})
+	apis = append(apis, &base.ApiWorker{Power: invokeReports, Do: this_.invokeReports})
+	apis = append(apis, &base.ApiWorker{Power: invokeStop, Do: this_.invokeStop})
+	apis = append(apis, &base.ApiWorker{Power: invokeInfo, Do: this_.invokeInfo})
 	apis = append(apis, &base.ApiWorker{Power: closePower, Do: this_.close})
 
 	return
@@ -60,6 +71,13 @@ type BaseRequest struct {
 	Args          []string `json:"args"`
 	ServerAddress string   `json:"serverAddress"`
 	Reload        bool     `json:"reload"`
+	TaskKey       string   `json:"taskKey"`
+	ToolboxId     int64    `json:"toolboxId,omitempty"`
+	IsTest        bool     `json:"isTest"`
+	Worker        int      `json:"worker"`
+	Duration      int      `json:"duration"`
+	Frequency     int      `json:"frequency"`
+	Timeout       int      `json:"timeout"`
 }
 
 func (this_ *api) context(requestBean *base.RequestBean, c *gin.Context) (res interface{}, err error) {
@@ -148,6 +166,7 @@ func (this_ *api) invokeByServerAddress(requestBean *base.RequestBean, c *gin.Co
 
 	data := map[string]interface{}{}
 	res = data
+	data["isTest"] = request.IsTest
 	data["start"] = time.Now().UnixMilli()
 	defer func() {
 		data["end"] = time.Now().UnixMilli()
@@ -173,22 +192,293 @@ func (this_ *api) invokeByServerAddress(requestBean *base.RequestBean, c *gin.Co
 		err = errors.New("args json " + argsJSON + " to args error:" + err.Error())
 		return
 	}
-
-	param, err := service.InvokeByServerAddress(request.ServerAddress, filename, request.ServiceName, request.MethodName, args...)
-	if param != nil {
-		data["writeStart"] = param.WriteStart.UnixMilli()
-		data["writeEnd"] = param.WriteEnd.UnixMilli()
-		data["readStart"] = param.ReadStart.UnixMilli()
-		data["readEnd"] = param.ReadEnd.UnixMilli()
-		var result = map[string]interface{}{}
-		result["args"] = param.Args
-		result["result"] = param.Result
-		result["exceptions"] = param.Exceptions
-		bs, e := json.MarshalIndent(result, "", "  ")
-		if e == nil {
-			data["result"] = string(bs)
+	if !request.IsTest {
+		var param *thrift.MethodParam
+		param, err = service.InvokeByServerAddress(request.ServerAddress, filename, request.ServiceName, request.MethodName, args...)
+		if param != nil {
+			data["writeStart"] = param.WriteStart.UnixMilli()
+			data["writeEnd"] = param.WriteEnd.UnixMilli()
+			data["readStart"] = param.ReadStart.UnixMilli()
+			data["readEnd"] = param.ReadEnd.UnixMilli()
+			var result = map[string]interface{}{}
+			result["args"] = param.Args
+			result["result"] = param.Result
+			result["exceptions"] = param.Exceptions
+			bs, e := json.MarshalIndent(result, "", "  ")
+			if e == nil {
+				data["result"] = string(bs)
+			}
 		}
+	} else {
+		var dir string
+		dir, err = this_.invokeTaskDir(request)
+		if err != nil {
+			return
+		}
+		var t *task.Task
+
+		executor := &invokeExecutor{
+			BaseRequest:  request,
+			filename:     filename,
+			args:         args,
+			workerClient: make(map[int]*thrift.ServiceClient),
+			service:      service,
+		}
+		t, err = task.New(&task.Options{
+			Key:       util.GetUUID(),
+			Worker:    request.Worker,
+			Frequency: request.Frequency,
+			Duration:  request.Duration,
+			Executor:  executor,
+		})
+		if err != nil {
+			return
+		}
+		taskDir := dir + "" + t.Key
+		_ = this_.saveTaskInfo(taskDir, t)
+		go func() {
+			defer func() {
+				_ = this_.saveTaskInfo(taskDir, t)
+				executor.stop()
+			}()
+			for !t.IsEnd {
+				_ = this_.saveTaskInfo(taskDir, t)
+				time.Sleep(time.Second * 1)
+			}
+		}()
+		go t.Run()
+		addTask(t)
 	}
+	return
+}
+
+type invokeExecutor struct {
+	*BaseRequest
+	filename         string
+	args             []interface{}
+	workerClient     map[int]*thrift.ServiceClient
+	workerClientLock sync.Mutex
+	service          *thrift.Workspace
+}
+
+func (this_ *invokeExecutor) stop() {
+	this_.workerClientLock.Lock()
+	defer this_.workerClientLock.Unlock()
+
+	for _, client := range this_.workerClient {
+		client.Stop()
+	}
+
+}
+func (this_ *invokeExecutor) getClient(param *task.ExecutorParam) (client *thrift.ServiceClient, err error) {
+	this_.workerClientLock.Lock()
+	defer this_.workerClientLock.Unlock()
+
+	client = this_.workerClient[param.WorkerIndex]
+	if client != nil {
+		return
+	}
+
+	client, err = thrift.NewServiceClientByAddress(this_.ServerAddress)
+	if err != nil {
+		return
+	}
+	this_.workerClient[param.WorkerIndex] = client
+	return
+}
+
+func (this_ *invokeExecutor) Before(param *task.ExecutorParam) (err error) {
+	_, err = this_.getClient(param)
+	if err != nil {
+		return
+	}
+
+	methodParam, err := this_.service.GetMethodParam(this_.filename, this_.ServiceName, this_.MethodName, this_.args...)
+	if err != nil {
+		return
+	}
+	param.Extend = methodParam
+
+	//util.Logger.Info("test Before", zap.Any("param", param))
+	return
+}
+
+func (this_ *invokeExecutor) Execute(param *task.ExecutorParam) (err error) {
+	client, err := this_.getClient(param)
+	if err != nil {
+		return
+	}
+	//util.Logger.Info("test Execute", zap.Any("param", param))
+	methodParam := param.Extend.(*thrift.MethodParam)
+
+	_, err = client.Send(context.Background(), methodParam)
+	return
+}
+
+func (this_ *invokeExecutor) After(param *task.ExecutorParam) (err error) {
+	//util.Logger.Info("test After", zap.Any("param", param))
+	return
+}
+
+func (this_ *api) invokeTaskDir(request *BaseRequest) (taskDir string, err error) {
+	taskDir = this_.toolboxService.GetFilesDir()
+	taskDir += fmt.Sprintf("%s/toolbox-%d/%s", "thrift-tasks", request.ToolboxId, request.RelativePath) + "/" + request.ServiceName + "/" + request.MethodName + "/"
+
+	ex, err := util.PathExists(taskDir)
+	if err != nil {
+		return
+	}
+	if !ex {
+		err = os.MkdirAll(taskDir, fs.ModePerm)
+	}
+
+	return
+}
+
+func (this_ *api) saveTaskInfo(taskDir string, task *task.Task) (err error) {
+	ex, err := util.PathExists(taskDir)
+	if err != nil {
+		return
+	}
+	if !ex {
+		err = os.MkdirAll(taskDir, fs.ModePerm)
+	}
+	bs, _ := json.MarshalIndent(task, "", "  ")
+	err = util.WriteFile(taskDir+"/task.json", bs)
+	if err != nil {
+		return
+	}
+	bs, _ = json.MarshalIndent(task.Metric.Count(), "", "  ")
+	err = util.WriteFile(taskDir+"/metric.json", bs)
+	if err != nil {
+		return
+	}
+	return
+}
+func (this_ *api) loadTask(taskDir string) (data map[string]interface{}, err error) {
+	defer func() {
+		if len(data) == 0 {
+			data = nil
+		}
+	}()
+	data = map[string]interface{}{}
+
+	var bs []byte
+	if ex, _ := util.PathExists(taskDir + "/task.json"); ex {
+		if bs, err = os.ReadFile(taskDir + "/task.json"); err != nil {
+			return
+		}
+		t := map[string]interface{}{}
+		err = util.JSONDecodeUseNumber(bs, &t)
+		if err != nil {
+			return
+		}
+		data["task"] = t
+	}
+	if ex, _ := util.PathExists(taskDir + "/metric.json"); ex {
+		if bs, err = os.ReadFile(taskDir + "/metric.json"); err != nil {
+			return
+		}
+		metric := map[string]interface{}{}
+		err = util.JSONDecodeUseNumber(bs, &metric)
+		if err != nil {
+			return
+		}
+		data["metric"] = metric
+	}
+
+	return
+}
+
+func (this_ *api) invokeReports(requestBean *base.RequestBean, c *gin.Context) (res interface{}, err error) {
+
+	request := &BaseRequest{}
+	if !base.RequestJSON(request, c) {
+		return
+	}
+
+	dir, err := this_.invokeTaskDir(request)
+	if err != nil {
+		return
+	}
+
+	fileList, err := os.ReadDir(dir)
+	if err != nil {
+		return
+	}
+	var taskInfo map[string]interface{}
+	var taskList []map[string]interface{}
+	for _, f := range fileList {
+		if !f.IsDir() {
+			continue
+		}
+		taskKey := f.Name()
+		taskInfo, err = this_.loadTask(dir + f.Name())
+		if err != nil {
+			return
+		}
+		if taskInfo != nil {
+			taskInfo["taskKey"] = taskKey
+			taskList = append(taskList, taskInfo)
+		}
+
+	}
+	res = taskList
+	return
+}
+
+var taskCache = map[string]*task.Task{}
+var taskLocker = &sync.Mutex{}
+
+func getTask(taskKey string) *task.Task {
+	taskLocker.Lock()
+	defer taskLocker.Unlock()
+
+	return taskCache[taskKey]
+}
+
+func addTask(task *task.Task) {
+	taskLocker.Lock()
+	defer taskLocker.Unlock()
+
+	taskCache[task.Key] = task
+}
+
+func removeTask(taskKey string) {
+	taskLocker.Lock()
+	defer taskLocker.Unlock()
+
+	delete(taskCache, taskKey)
+}
+
+func (this_ *api) invokeStop(requestBean *base.RequestBean, c *gin.Context) (res interface{}, err error) {
+	request := &BaseRequest{}
+	if !base.RequestJSON(request, c) {
+		return
+	}
+
+	t := getTask(request.TaskKey)
+
+	if t != nil {
+		t.Stop()
+	}
+	return
+}
+
+func (this_ *api) invokeInfo(requestBean *base.RequestBean, c *gin.Context) (res interface{}, err error) {
+
+	request := &BaseRequest{}
+	if !base.RequestJSON(request, c) {
+		return
+	}
+
+	dir, err := this_.invokeTaskDir(request)
+	if err != nil {
+		return
+	}
+
+	res, err = this_.loadTask(dir + request.TaskKey)
+
 	return
 }
 
